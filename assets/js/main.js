56903fd1132957498ebb272603a9186db72ad2ff
mywaiting <ourwaiting@gmail.com>
2020-07-08 00:41:35 +0800

    在 SF 上找到一个神奇的使用浏览器特性检测的脚本实现，牛逼啊~

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,44 @@
 /* (c) 2020 Keptab. All rights reserved */
+
+
+// 使用特性检测而不是 User-Agent 来检测浏览器
+// 参考链接 https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+
+var Browser = (function() {
+
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+
+// Chrome 1 - 79
+var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+// Edge (based on chromium) detection
+var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+return {
+    isOpera: isOpera,
+    isFirefox: isFirefox,
+    isSafari: isSafari,
+    isIE: isIE,
+    isEdge: isEdge,
+    isEdgeChromium: isEdgeChromium,
+    isBlink: isBlink
+}
+
+})();
+
